ff11f780818240ceeb9d8702a968040b6072ea31
Truong-Giang-801 <95930216+Truong-Giang-801@users.noreply.github.com>
2024-09-08 19:38:47 +0700

    Delete Vou.Services.VoucherAPI directory

--- Vou.Services.VoucherAPI/Models/Dto/ResponeDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Vou.Services.VoucherAPI.Models.Dto
-{
-	public class ResponeDto
-	{
-		public object? Result { get; set; }
-		public bool? IsSuccess { get; set; } = true;
-		public string Message { get; set; } = string.Empty;
-	}
-}
