39de93b6e8a9c6ffcb708c103134afcb1e4655ce
Truong-Giang-801 <giangle8013@gmail.com>
2024-09-09 23:39:32 +0700

    update game api endpoint

--- Vou.Services.GameAPI/obj/Debug/net8.0/Vou.Services.GameAPI.AssemblyInfo.cs
+++ Vou.Services.GameAPI/obj/Debug/net8.0/Vou.Services.GameAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Vou.Services.GameAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9c4fd86f0908a617943235a4b63d8479e50deee")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd32a15337f7d0ee8fb7893255c25f855fc1b995")]
 [assembly: System.Reflection.AssemblyProductAttribute("Vou.Services.GameAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Vou.Services.GameAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
